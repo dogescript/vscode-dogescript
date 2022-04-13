d2558f9fd61d99d73f9de65c904a08c04c082136
Jan Monterrubio <janmonterrubio@gmail.com>
2022-04-12 23:42:44 -0500

    more test scaffold

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -5,10 +5,10 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
+suite('Extension Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
+	test('Sample', () => {
 		assert.strictEqual([1, 2, 3].indexOf(5), -1);
 		assert.strictEqual([1, 2, 3].indexOf(0), -1);
 	});
